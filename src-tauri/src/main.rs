a88d95cd12458b529ebd18cd3ceebe2d525e61ba
BrookDev <brooklynbaylisdev@gmail.com>
2024-04-09 23:50:47 +0100

    File / Folder Selection
    feat!: added ability to select files / directories

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rfd::FileDialog;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn select_file() -> String {
+    let file = FileDialog::new()
+        .add_filter("images", &["avif", "bmp", "gif", "ico", "jpg", "png"])
+        .set_directory("/")
+        .pick_file();
+
+    let string_path = file.unwrap_or_default().to_string_lossy().to_string();
+
+    string_path
+}
+
+#[tauri::command]
+fn select_directory() -> String {
+    let file = FileDialog::new()
+        .set_directory("/")
+        .pick_folder();
+
+    let string_path = file.unwrap_or_default().to_string_lossy().to_string();
+
+    string_path
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![select_file, select_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,31 @@ let targetFileInputEl: HTMLInputElement | null;
 let targetFileButtonEl: HTMLButtonElement | null;
 let outputDirInputEl: HTMLInputElement | null;
 let outputDirButtonEl: HTMLButtonElement | null;
-let outputNameInputEl: HTMLButtonElement | null;
+let outputNameInputEl: HTMLInputElement | null;
 let outputTypeDropdownEl: HTMLSelectElement | null;
 
 let convertButtonEl: HTMLButtonElement | null;
 let clearButtonEl: HTMLButtonElement | null;
+
+function sanitiseInput(inputEl: HTMLInputElement | null, regex: RegExp) {
+	if (inputEl?.value) {
+		inputEl.value = inputEl.value.replace(regex, "");
+	}
+}
+
+async function selectTargetFile() {
+	if (targetFileInputEl) {
+		let filePath: string = await invoke("select_file");
+		targetFileInputEl.value = filePath ? filePath : targetFileInputEl.value;
+	}
+}
+
+async function selectOutputDirectory() {
+	if (outputDirInputEl) {
+		let dirPath: string = await invoke("select_directory");
+		outputDirInputEl.value = dirPath ? dirPath : outputDirInputEl.value;
+	}
+}
 
 async function clearInputs() {
 	if (targetFileInputEl && outputDirInputEl && outputNameInputEl) {
@@ -28,6 +48,28 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	convertButtonEl = document.querySelector("#convert-btn");
 	clearButtonEl = document.querySelector("#clear-btn");
+
+	targetFileButtonEl?.addEventListener("click", (e) => {
+		e.preventDefault();
+		selectTargetFile();
+	});
+
+	targetFileInputEl?.addEventListener("input", (_) => {
+		sanitiseInput(targetFileInputEl, /^[a-z]:((\\|\/)[a-z0-9\s_@^!#$%&+={}[\]-]+)$/i);
+	});
+
+	outputDirInputEl?.addEventListener("input", (_) => {
+		sanitiseInput(outputDirInputEl, /^[a-z]:((\\|\/)[a-z0-9\s_@^!#$%&+={}[\]-]+)$/i);
+	});
+
+	outputNameInputEl?.addEventListener("input", (_) => {
+		sanitiseInput(outputNameInputEl, /[^a-zA-Z0-9 ]/g);
+	});
+
+	outputDirButtonEl?.addEventListener("click", (e) => {
+		e.preventDefault();
+		selectOutputDirectory();
+	});
 
 	clearButtonEl?.addEventListener("click", (e) => {
 		e.preventDefault();
