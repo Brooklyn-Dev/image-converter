920f9710ab056070916702fe37198ba9b0ab2d35
BrookDev <brooklynbaylisdev@gmail.com>
2024-04-10 20:00:00 +0100

    Input Validation & Result Message
    feat!: Added input validation for input fields
    feat: Added coloured result messages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::Path;
+
 use rfd::FileDialog;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -28,9 +30,28 @@ fn select_directory() -> String {
     string_path
 }
 
+#[tauri::command]
+fn convert_image_file(img_path: &str, out_dir: &str, out_name: &str, out_type: &str) -> (String, bool) {
+    let img_path = Path::new(img_path);
+    let out_dir = Path::new(out_dir);
+
+    print!("Success! {}.{} was created.", out_name, out_type.to_lowercase());
+    
+    if !img_path.is_file() {
+        return ("The path of the target file is invalid.".to_string(), false);
+    }
+    else if !out_dir.is_dir() {
+        return ("The path of the output directory is invalid.".to_string(), false);
+    }
+    
+    let result_message: String = format!("(testing) Success! {}.{} was created.", out_name, out_type.to_lowercase()).to_string();
+
+    return (result_message, true);
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![select_file, select_directory])
+        .invoke_handler(tauri::generate_handler![select_file, select_directory, convert_image_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ let outputDirButtonEl: HTMLButtonElement | null;
 let outputNameInputEl: HTMLInputElement | null;
 let outputTypeDropdownEl: HTMLSelectElement | null;
 
+let resultMessageEl: HTMLParagraphElement | null;
+
 let convertButtonEl: HTMLButtonElement | null;
 let clearButtonEl: HTMLButtonElement | null;
 
@@ -16,26 +18,51 @@ function sanitiseInput(inputEl: HTMLInputElement | null, regex: RegExp) {
 	}
 }
 
-async function selectTargetFile() {
-	if (targetFileInputEl) {
-		let filePath: string = await invoke("select_file");
-		targetFileInputEl.value = filePath ? filePath : targetFileInputEl.value;
+function setResultMessage(text: string, success: boolean) {
+	resultMessageEl!.textContent = text;
+
+	if (success) {
+		resultMessageEl?.classList.remove("u-red-text");
+		resultMessageEl?.classList.add("u-green-text");
+	} else {
+		resultMessageEl?.classList.add("u-red-text");
+		resultMessageEl?.classList.remove("u-green-text");
 	}
+}
+
+async function selectTargetFile() {
+	const filePath: string = await invoke("select_file");
+	targetFileInputEl!.value = filePath ? filePath : targetFileInputEl!.value;
 }
 
 async function selectOutputDirectory() {
-	if (outputDirInputEl) {
-		let dirPath: string = await invoke("select_directory");
-		outputDirInputEl.value = dirPath ? dirPath : outputDirInputEl.value;
-	}
+	const dirPath: string = await invoke("select_directory");
+	outputDirInputEl!.value = dirPath ? dirPath : outputDirInputEl!.value;
 }
 
-async function clearInputs() {
-	if (targetFileInputEl && outputDirInputEl && outputNameInputEl) {
-		targetFileInputEl.value = ""; // !.
-		outputDirInputEl.value = "";
-		outputNameInputEl.value = "";
+async function convertFile() {
+	if (!(targetFileInputEl!.value.trim() && outputDirInputEl!.value.trim() && outputNameInputEl!.value.trim())) {
+		setResultMessage("Fill in all the inputs before converting", false);
+		return;
 	}
+
+	const result: [resultMessage: string, success: boolean] = await invoke("convert_image_file", {
+		imgPath: targetFileInputEl!.value,
+		outDir: outputDirInputEl!.value,
+		outName: outputNameInputEl!.value,
+		outType: outputTypeDropdownEl!.value,
+	});
+
+	const [resultMessage, success] = result;
+	setResultMessage(resultMessage, success);
+}
+
+async function clear() {
+	targetFileInputEl!.value = "";
+	outputDirInputEl!.value = "";
+	outputNameInputEl!.value = "";
+
+	resultMessageEl!.textContent = "";
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -44,7 +71,9 @@ window.addEventListener("DOMContentLoaded", () => {
 	outputDirInputEl = document.querySelector("#out-dir-input");
 	outputDirButtonEl = document.querySelector("#out-dir-btn");
 	outputNameInputEl = document.querySelector("#out-name-input");
-	outputTypeDropdownEl = document.querySelector("#out-name-btn");
+	outputTypeDropdownEl = document.querySelector("#out-name-dropdown");
+
+	resultMessageEl = document.querySelector("#result-msg");
 
 	convertButtonEl = document.querySelector("#convert-btn");
 	clearButtonEl = document.querySelector("#clear-btn");
@@ -71,8 +100,13 @@ window.addEventListener("DOMContentLoaded", () => {
 		selectOutputDirectory();
 	});
 
+	convertButtonEl?.addEventListener("click", (e) => {
+		e.preventDefault();
+		convertFile();
+	});
+
 	clearButtonEl?.addEventListener("click", (e) => {
 		e.preventDefault();
-		clearInputs();
+		clear();
 	});
 });
